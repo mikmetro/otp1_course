b9fafd070c7cb87e76c7cf57b2107b72bd369d11
Mikko Varis <mikkomav@metropolia.fi>
2025-09-18 09:51:55 +0300

    Add tempconverter

---
+++ src/main/java/TempConverter/TemperatureConverter.java
@@ -0,0 +1,15 @@
+package TempConverter;
+
+public class TemperatureConverter {
+    public static double fahrenheitToCelsius(double fahrenheit) {
+        return (fahrenheit - 32.0) * (5.0 / 9.0);
+    }
+
+    public static double celsiusToFahrenheit(double celsius) {
+        return (celsius * (9.0 / 5.0)) + 32;
+    }
+
+    public static boolean isExtremeTemperature(double celsius) {
+        return celsius < -40 || celsius > 50;
+    }
+}
---
+++ src/test/java/TempConverter/TemperatureConverterTest.java
@@ -0,0 +1,20 @@
+package TempConverter;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+public class TemperatureConverterTest extends TestCase {
+    public void testCelsiusToFahrenheit() {
+        assertEquals(86, TemperatureConverter.celsiusToFahrenheit(30), 0.0001);
+    }
+
+    public void testFahrenheitToCelsius() {
+        assertEquals(54.44444, TemperatureConverter.celsiusToFahrenheit(130), 0.0001);
+    }
+
+    public void testExtremeTemp() {
+        assertTrue(TemperatureConverter.isExtremeTemperature(-41));
+        assertFalse(TemperatureConverter.isExtremeTemperature(0));
+        assertTrue(TemperatureConverter.isExtremeTemperature(50.1));
+    }
+}
